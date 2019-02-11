1d3d75177189bc9f50f73195da69f62b9f9a3bbd
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-11 11:01:41 +0530

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
-	int n,i;
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{ 
-		printf("Hello World ");
-	}
-	return 0;
+int n,i;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{ 
+return 0;
+}
 }
