2847a1b5b138b78144cbfb79ec3e66dc0863e223
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-13 14:04:20 +0530

    Update and rename positive or negative.c++ to positive or negative.c

---
+++ positive or negative.c
@@ -0,0 +1,17 @@
+#include<conio.h>
+void main()
+{
+ clrscr();
+ printf("enter the positive");
+ scanf("%d",positive);
+ if(i<n)
+ {
+  printf("enter the negative);
+  scanf("%d",negative);
+         }
+   else
+         {
+printf("enter zero");
+          scanf("%d",zero);
+         }
+         }
