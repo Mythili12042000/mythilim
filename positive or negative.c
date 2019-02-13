e7b5f8d4c1abf47776de9cbbc6c3b3b3682e218f
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-13 13:48:49 +0530

    Update positive or negative.c

--- positive or negative.c
+++ positive or negative.c
@@ -2,17 +2,13 @@
   
 int main() 
 { 
-    int A; 
-  
-    printf("Enter the number A: "); 
+   int a; 
     scanf("%d", &A); 
-  
-    if (A > 0) 
+  if (A > 0) 
         printf("%d is positive.", A); 
     else if (A < 0) 
         printf("%d is negative.", A); 
     else if (A == 0) 
         printf("%d is zero.", A); 
-  
-    return 0; 
+ return 0; 
 } 
