da5cb7546f21ed520cddd0c3aba5e25d13eb3f98
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-13 13:43:06 +0530

    Update positive or negative.c

--- positive or negative.c
+++ positive or negative.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    double number;
-
-    printf("Enter a number: ");
-    scanf("%lf", &number);
-
-    if (number <= 0.0)
-    {
-        if (number == 0.0)
-            printf("You entered 0.");
-        else
-            printf("You entered a negative number.");
-    }
-    else
-        printf("You entered a positive number.");
-    return 0;
+ double number;
+ scanf("%lf", &number);
+if (number <= 0.0)
+{
+if (number == 0.0)
+ else
+     return 0;
 }
