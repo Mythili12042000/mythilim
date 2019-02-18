57f5a25316a6a93a4f653e4fe4bc0a65211dc97a
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-18 10:57:04 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int  main() {
+    int first, diff, terms, value, sum=0, i;
+ scanf("%d", &terms);
+value = first;
+   for(i = 0; i < terms; i++) {
+value = value + diff;
+    }
+  getch();
+ return 0;
+}
