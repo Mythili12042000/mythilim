2ef49fa8d105058d3df6219211a27e590598b2ca
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-18 10:03:55 +0530

    Create power of n number.c

---
+++ power of n number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int base, exponent;
+ long long result = 1;
+ scanf("%d", &base);
+scanf("%d", &exponent);
+ while (exponent != 0)
+    {
+        result *= base;
+        --exponent;
+    }
+return 0;
+}
