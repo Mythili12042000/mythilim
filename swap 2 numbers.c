d2a3f48cfce82cdf119adf18285aac17789958c3
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-20 14:30:32 +0530

    Create swap 2 numbers.c

---
+++ swap 2 numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+      double firstNumber, secondNumber, temporaryVariable;
+      scanf("%lf", &firstNumber);
+      scanf("%lf",&secondNumber);
+      firstNumber = secondNumber;
+      return 0;
+}
