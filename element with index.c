d7617e30b6e46e9332a603b834161334f7c5b755
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-20 14:06:35 +0530

    Create element with index.c

---
+++ element with index.c
@@ -0,0 +1,16 @@
+#include <iostream> 
+#include <algorithm> 
+using namespace std; 
+  
+void printDistinct(int arr[], int n) 
+{ 
+  for (int i=0; i<n; i++) 
+    { 
+       int j; 
+        for (j=0; j<i; j++) 
+           if (arr[i] == arr[j]) 
+               break;
+        if (i == j) 
+          cout << arr[i] << " "; 
+    } 
+} 
