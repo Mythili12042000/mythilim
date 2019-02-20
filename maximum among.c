b6019413be9a80d3bc05564d5943a800d9df7178
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-20 14:34:34 +0530

    Create maximum among.c

---
+++ maximum among.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    double n1, n2, n3;
+    scanf("%lf %lf %lf", &n1, &n2, &n3);
+
+    if( n1>=n2 && n1>=n3 )
+if( n2>=n1 && n2>=n3 )
+      if( n3>=n1 && n3>=n2 )
+      return 0;
+}
