316cd0e2c396eb1d2b91c13170f6df8a923855ab
Mythili12042000 <43204309+Mythili12042000@users.noreply.github.com>
2019-02-20 14:00:16 +0530

    Create 24 merge.c

---
+++ 24 merge.c
@@ -0,0 +1,12 @@
+void printArray(int A[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+       } 
+int main() 
+{ 
+    int arr[] = {12, 11, 13, 5, 6, 7}; 
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+   mergeSort(arr, 0, arr_size - 1); 
+   return 0; 
+} 
